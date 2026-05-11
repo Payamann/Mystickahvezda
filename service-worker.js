fd3ccba9444b5d65526c10841af1b369ea622b24
Pavel <pavel@local>
2026-05-11 15:45:06 +0200

    Compact mobile cookie banner on signup

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Provides offline caching with stale-while-revalidate strategy
  */
 
-const CACHE_NAME = 'mysticka-hvezda-385b3a47a85a';
+const CACHE_NAME = 'mysticka-hvezda-8a3f2f5faccb';
 const MAX_RUNTIME_CACHE_SIZE = 150;
 const STATIC_ASSETS = [
     '/fonts/local-fonts.css',
