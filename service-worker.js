f3dc3b844a2f14f5b47af1323b5b084b5e5a425e
Pavel <pavel@local>
2026-05-14 00:05:54 +0200

    Update service worker cache for revenue fixes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Provides offline caching with stale-while-revalidate strategy
  */
 
-const CACHE_NAME = 'mysticka-hvezda-3b5a5793de4a';
+const CACHE_NAME = 'mysticka-hvezda-15ac6eb47635';
 const MAX_RUNTIME_CACHE_SIZE = 150;
 const STATIC_ASSETS = [
     '/fonts/local-fonts.css',
